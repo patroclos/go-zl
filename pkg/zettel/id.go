c27cbfd2c60c8b555e54c86f93ed13de28e07cca
Joshua Jensch <joshua@jensch.dev>
2022-01-27 21:26:11 +0100

    change id format to YYMMDD-RAND

--- pkg/zettel/id.go
+++ pkg/zettel/id.go
@@ -1,28 +1,30 @@
 package zettel
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 const (
 	idCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	idLen     = 12
+	idLen     = 4
 )
 
 var rng *rand.Rand
 
-func generateId(rng *rand.Rand) string {
+func generateId(rng *rand.Rand, t time.Time) string {
+	ts := t.Format("060102")
 	buf := [idLen]byte{}
 	for i := 0; i < idLen; i++ {
 		buf[i] = idCharset[rng.Intn(len(idCharset))]
 	}
-	return string(buf[:])
+	return fmt.Sprintf("%s-%s", ts, buf[:])
 }
 
 func plainGenerateId() string {
 	if rng == nil {
 		rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 	}
-	return generateId(rng)
+	return generateId(rng, time.Now())
 }
